cabedc3c2a38e6845bda8f5995821c9f9874ba2f
Yaang Vu <yaangvu@gmail.com>
2021-07-16 11:17:15 +0700

    Add provider

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace YaangVu\SisModel\App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use YaangVu\SisModel\App\Models\impl\UserNoSQL;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Boot the authentication services for the application.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Here you may define how you wish users to be authenticated for your Lumen
+        // application. The callback which receives the incoming request instance
+        // should return either a UserNoSQL instance or null. You're free to obtain
+        // the UserNoSQL instance via an API token or any other method necessary.
+
+        $this->app['auth']->viaRequest('api', function ($request) {
+            $uuid = $request->header('X-user-uuid');
+            if ($uuid)
+                return (new UserNoSQL())->where('uuid', $uuid)->first();
+            else
+                return new UserNoSQL();
+        });
+    }
+}
