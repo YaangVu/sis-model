47494363324e0c945f9d42b0f37b231007fc4258
Your NamePhuongNam160899 <you@example.comhaphuongnam81999@gmail.com>
2021-08-04 12:07:33 +0700

    created table device token

---
+++ database/migrations/2021_08_04_033732_create_device_token_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDeviceTokenTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('device_token', function (Blueprint $table) {
+            $table->id();
+            $table->string('type');
+            $table->unsignedBigInteger('user_id');
+            $table->string('device_token')->unique();
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('device_token');
+    }
+}
