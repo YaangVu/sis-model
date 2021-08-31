05785e2f3c8f49730adc15f54593777c921ad882
tungnd <tungnd@toprate.io>
2021-08-31 10:30:04 +0700

    [Update] migrate cpa

--- app/Models/impl/GpaSQL.php
+++ app/Models/impl/GpaSQL.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Support\Carbon;git
+use Illuminate\Support\Carbon;
 use YaangVu\Constant\CodeConstant;
 use YaangVu\Constant\DbConnectionConstant;
 use YaangVu\SisModel\App\Models\Gpa;
