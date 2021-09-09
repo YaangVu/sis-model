6a0f0f5a8c3aa8f76b287797dbf009fbb415633b
tungnd <tungnd@toprate.io>
2021-09-09 17:02:59 +0700

    [Update] grade_id to table CPA

--- database/factories/CpaFactory.php
+++ database/factories/CpaFactory.php
@@ -5,6 +5,7 @@ namespace YaangVu\SisModel\Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use JetBrains\PhpStorm\ArrayShape;
 use YaangVu\SisModel\App\Models\impl\CpaSQL;
+use YaangVu\SisModel\App\Models\impl\GradeSQL;
 use YaangVu\SisModel\App\Models\impl\ProgramSQL;
 use YaangVu\SisModel\App\Models\impl\UserSQL;
 
@@ -15,11 +16,13 @@ class CpaFactory extends Factory
 
     #[ArrayShape(['uuid'           => "string", 'user_id' => "int", 'school_id' => "int",
                   'learned_credit' => "int", 'earned_credit' => "int", 'rank' => "int",
+                  "grade_id"       => "int",
                   "program_id"     => "int", 'cpa' => "float", 'bonus_cpa' => "float"])]
     public function definition(): array
     {
         $users    = UserSQL::all('id')->pluck('id')->toArray();
         $programs = ProgramSQL::all()->pluck('id')->toArray();
+        $grades    = GradeSQL::all()->pluck('id')->toArray();
 
         return [
             'uuid'           => $this->faker->uuid,
@@ -31,6 +34,7 @@ class CpaFactory extends Factory
             'cpa'            => $this->faker->randomFloat(2, 0, 4),
             'bonus_cpa'      => $this->faker->randomFloat(2, 0, 1),
             'rank'           => $this->faker->numberBetween(1, 1000),
+            'grade_id'       => $this->faker->randomElement($grades),
         ];
     }
 }
