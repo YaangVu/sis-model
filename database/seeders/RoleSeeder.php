160a9dcb64505aca0def44965ade23f539ae2013
Hoang Viet <hoangviet10172001@gmail.com>
2021-09-15 16:52:34 +0700

    add column priority table role

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -21,16 +21,16 @@ class RoleSeeder extends Seeder
         $data    = [];
         $schools = SchoolSQL::all();
         $roles   = [
-            ['name' => RoleConstant::ADMIN, 'group' => RoleConstant::STAFF],
-            ['name' => RoleConstant::TEACHING_ASSISTANT, 'group' => RoleConstant::STAFF],
-            ['name' => RoleConstant::TEACHER, 'group' => RoleConstant::STAFF],
+            ['name' => RoleConstant::ADMIN, 'group' => RoleConstant::STAFF,'priority' => 2],
+            ['name' => RoleConstant::TEACHING_ASSISTANT, 'group' => RoleConstant::STAFF,],
+            ['name' => RoleConstant::TEACHER, 'group' => RoleConstant::STAFF,'priority' => 5],
             ['name' => RoleConstant::HEADTEACHER, 'group' => RoleConstant::STAFF],
             ['name' => RoleConstant::PRINCIPAL, 'group' => RoleConstant::STAFF],
             ['name' => RoleConstant::ACADEMIC_COORDINATOR, 'group' => RoleConstant::STAFF],
-            ['name' => RoleConstant::STUDENT, 'group' => RoleConstant::STUDENT_AND_FAMILY],
-            ['name' => RoleConstant::FAMILY, 'group' => RoleConstant::STUDENT_AND_FAMILY],
-            ['name' => RoleConstant::COUNSELOR, 'group' => RoleConstant::STAFF],
-            ['name' => RoleConstant::GOD, 'group' => null],
+            ['name' => RoleConstant::STUDENT, 'group' => RoleConstant::STUDENT_AND_FAMILY,'priority' => 13],
+            ['name' => RoleConstant::FAMILY, 'group' => RoleConstant::STUDENT_AND_FAMILY, 'priority' => 13],
+            ['name' => RoleConstant::COUNSELOR, 'group' => RoleConstant::STAFF,'priority' => 8],
+            ['name' => RoleConstant::GOD, 'group' => null,'priority' => 1],
         ];
         foreach ($schools as $school) {
             foreach ($roles as $role) {
