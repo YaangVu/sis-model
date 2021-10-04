605b6ccecbabecbe396c9df8582d398d1e8297d9
keyhoang <hoangky@toprate.io>
2021-10-04 10:46:30 +0700

    update excel permission

--- seed.sh
+++ seed.sh
@@ -5,3 +5,5 @@ php artisan migrate:refresh
 sleep 10;
 
 php artisan db:seed --class="YaangVu\SisModel\Database\Seeders\DatabaseSeeder"
+
+php artisan db:seed --class="YaangVu\SisModel\Database\Seeders\PermissionSeeder"
