d2c3bc513d78bb16e643e7833b992d85f4bb3494
Hoang Viet <hoangviet10172001@gmail.com>
2022-05-06 17:20:50 +0700

    add model progress report

---
+++ app/Models/ProgressReport.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @Author yaangvu
+ * @Date   Apr 21, 2022
+ */
+
+namespace YaangVu\SisModel\App\Models;
+
+interface ProgressReport 
+{
+    public const table = 'progress_reports';
+}
