69112fad8f49f7530427d1925cccd594307490c3
Hoang Viet <hoangviet10172001@gmail.com>
2022-05-09 09:21:31 +0700

    add model progressSetting

---
+++ app/Models/ProgressSetting.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @Author yaangvu
+ * @Date   Apr 21, 2022
+ */
+
+namespace YaangVu\SisModel\App\Models;
+
+interface ProgressSetting
+{
+    public const table = 'progress_settings';
+}
