dab33ac48a9edf506c374f2a62802314c9a8714f
dung doan <doanvietdung0103@gmail.com>
2022-05-30 10:33:42 +0700

    update SurveyNoSql , create model survey answer NoSql

---
+++ app/Models/SurveyAnswer.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @Author Edogawa Conan
+ * @Date   May 30, 2022
+ */
+
+namespace YaangVu\SisModel\App\Models;
+
+interface SurveyAnswer
+{
+    public const table = 'survey_answers';
+
+}
