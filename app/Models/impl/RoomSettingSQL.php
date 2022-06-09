79d4be8cfb2cf9a7fee42ebb7b1b37f4a63fad82
Hoang Viet <hoangviet10172001@gmail.com>
2022-06-09 15:13:31 +0700

    add migration table room_settings and add model room_settings

---
+++ app/Models/impl/RoomSettingSQL.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @Author apple
+ * @Date   Jun 09, 2022
+ */
+
+namespace YaangVu\SisModel\App\Models\impl;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Jenssegers\Mongodb\Eloquent\Model;
+use YaangVu\Constant\DbConnectionConstant;
+use YaangVu\SisModel\App\Models\RoomSetting;
+
+
+/**
+ * YaangVu\SisModel\App\Models\RoomSettingSQL
+ *
+ * @property int         $id
+ * @property string|null $email
+ * @property string|null $password
+ * @method static Builder|RoomSettingSQL whereEmail($value)
+ */
+class RoomSettingSQL extends Model implements RoomSetting
+{
+    use HasFactory;
+
+    protected $fillable
+        = [
+            'email',
+            'password',
+            'sc_id'
+        ];
+
+    protected $table = self::table;
+
+    protected $connection = DbConnectionConstant::SQL;
+}
