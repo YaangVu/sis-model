9b148634ebd8d547a614c2eee1e0a3f05868ace4
Hoang Viet <hoangviet10172001@gmail.com>
2022-06-10 15:31:14 +0700

    add table room_settings

--- database/migrations/2022_06_10_082923_create_table_room_settings.php
+++ database/migrations/2022_06_10_082923_create_table_room_settings.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTableRoomSettingsTable extends Migration
+class CreateTableRoomSettings extends Migration
 {
     /**
      * Run the migrations.
@@ -15,8 +15,9 @@ class CreateTableRoomSettingsTable extends Migration
     {
         Schema::create('room_settings', function (Blueprint $table) {
             $table->id();
-            $table->string('email');
+            $table->string('account');
             $table->string('password');
+            $table->integer('priority');
             $table->string('sc_id');
             $table->timestamps();
         });
