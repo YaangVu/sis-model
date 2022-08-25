7d4b4bd28d32ee31f01119406da4a0664c50f70d
Pham Van Tien <tienpv1@toprate.io>
2022-08-25 14:53:30 +0700

    add school_id to zoom_meetings table

---
+++ database/migrations/2022_08_25_072731_add_school_id_to_zoom_meeting_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddSchoolIdToZoomMeetingTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('zoom_meetings', function (Blueprint $table) {
+            $table->unsignedBigInteger('school_id')->nullable();
+
+            $table->foreign('school_id')->references('id')->on('schools')
+                  ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('zoom_meetings', function (Blueprint $table) {
+            $table->dropColumn('school_id');
+        });
+    }
+}
