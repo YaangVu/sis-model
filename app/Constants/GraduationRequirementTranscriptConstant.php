9d5cc00891c832544faefb96e35b9e4ce5c789f9
Pham Van Tien <tienpv1@toprate.io>
2023-03-09 10:29:14 +0700

    add gradution requirement transcript constant

---
+++ app/Constants/GraduationRequirementTranscriptConstant.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @Author im.phien
+ * @Date   Mar 09, 2023
+ */
+
+namespace YaangVu\SisModel\App\Constants;
+
+class GraduationRequirementTranscriptConstant
+{
+    const ENG     = 'ENG';
+    const MATH    = 'MATH';
+    const SCI     = 'SCI';
+    const SCO     = 'SCO';
+    const PE_HLTH = 'PE_HLTH';
+    const LANG    = 'LANG';
+    const ART     = 'ART';
+    const ELC     = 'ELC';
+
+    const ALL = [
+      self::ENG,
+      self::MATH,
+      self::SCI,
+      self::SCO,
+      self::PE_HLTH,
+      self::LANG,
+      self::ART,
+      self::ELC
+    ];
+}
