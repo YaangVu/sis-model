79f41bb45054a21589f2848f35f17e33c1679ac8
namhp <namhp>
2023-06-07 11:25:33 +0700

    add column term

--- database/migrations/2023_06_06_102228_add_attribute_into_terms_table.php
+++ database/migrations/2023_06_06_102228_add_attribute_into_terms_table.php
@@ -16,6 +16,7 @@ class AddAttributeIntoTermsTable extends Migration
         Schema::table('terms', function (Blueprint $table) {
             $table->string('school_year')->nullable();
             $table->string('semester_number')->nullable();
+            $table->unsignedBigInteger('link')->nullable();
         });
     }
 
@@ -29,6 +30,7 @@ class AddAttributeIntoTermsTable extends Migration
         Schema::table('terms', function (Blueprint $table) {
             $table->dropColumn('school_year');
             $table->dropColumn('semester_number');
+            $table->dropColumn('link');
         });
     }
 }
